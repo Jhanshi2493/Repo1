8b218a017d5b90427af490ca8c65a7a7cfc65b6c
Jhanshi2493 <48619193+Jhanshi2493@users.noreply.github.com>
2019-03-17 16:44:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  if x == "banana":
-    continue
-  print(x)
+public class Hello {
+    public static void main(String args[]){
+        System.out.println("I'm dancing");
+    }
+}
